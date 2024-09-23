eaabb9ffb7c1d624f9edcbfd3915e4a33c5cb630
Juan Becerra <juan.becerra@est.iudigital.edu.co>
2024-09-23 17:19:15 -0500

    Subiendo el proyecto al espacio de trabajo en GITHUB

---
+++ src/IUDIGITAL/Departamento.java
@@ -0,0 +1,8 @@
+package IUDIGITAL;
+
+public class Departamento {
+    private int idDepartamento;
+    private String Nombre;
+    private String ListaEmpleados;
+
+}
---
+++ src/IUDIGITAL/ReporteDesempeno.java
@@ -0,0 +1,8 @@
+package IUDIGITAL;
+
+public class ReporteDesempeno {
+    private int idReporte;
+    private String empleado;
+    private String fechaGeneracion;
+    private int metricas;
+}
