3afe9960c151c59ed03af92f5c3217f77ef4aeee
Juan Becerra <juan.becerra@est.iudigital.edu.co>
2024-09-23 16:52:10 -0500

    Subiendo el proyecto al espacio de trabajo en GITHUB

--- src/IUDIGITAL/Empleado.java
+++ src/IUDIGITAL/Empleado.java
@@ -1,4 +1,10 @@
 package IUDIGITAL;
 
 public class Empleado {
+    private int id_Empleado;
+    private String Nombre;
+    private String Apellido;
+    private String Tipo_Empleado;
+    private String Fecha_Contratacion;
+
 }
