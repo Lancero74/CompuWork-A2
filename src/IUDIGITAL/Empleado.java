d12e2a5f28505b003b5b86af51e76c11b2f420f7
yenis sarmiento cabarcas <yenis.sarmiento@est.iudigital.edu.co>
2024-09-24 10:45:25 -0500

    Update Empleado.java

--- src/IUDIGITAL/Empleado.java
+++ src/IUDIGITAL/Empleado.java
@@ -7,4 +7,59 @@ public class Empleado {
     private String Tipo_Empleado;
     private String Fecha_Contratacion;
 
+// constructor
+    public Empleado(int id_Empleado, String nombre,String Apellido,String Tipo_Empleado) {
+        this.id_Empleado = id_Empleado;
+        this.Nombre = nombre;
+        this.Apellido = Apellido;
+        this.Tipo_Empleado = Tipo_Empleado;
+        this.Fecha_Contratacion = Fecha_Contratacion;
+
+    }
+// constructor vacio
+    public Empleado() {
+
+    }
+    // Getters and Setters
+
+    public int getId_Empleado() {
+        return id_Empleado;
+    }
+
+    public void setId_Empleado(int id_Empleado) {
+        this.id_Empleado = id_Empleado;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getTipo_Empleado() {
+        return Tipo_Empleado;
+    }
+
+    public void setTipo_Empleado(String tipo_Empleado) {
+        Tipo_Empleado = tipo_Empleado;
+    }
+
+    public String getFecha_Contratacion() {
+        return Fecha_Contratacion;
+    }
+
+    public void setFecha_Contratacion(String fecha_Contratacion) {
+        Fecha_Contratacion = fecha_Contratacion;
+    }
+
 }
