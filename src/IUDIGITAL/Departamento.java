fe811411f9d8f5ad5fdf150bd592e64c7793e47c
yenis sarmiento cabarcas <yenis.sarmiento@est.iudigital.edu.co>
2024-09-24 10:48:02 -0500

    Update Departamento.java

--- src/IUDIGITAL/Departamento.java
+++ src/IUDIGITAL/Departamento.java
@@ -5,4 +5,39 @@ public class Departamento {
     private String Nombre;
     private String ListaEmpleados;
 
+// constructor
+public Departamento(int idDepartamento, String nombre, String listaEmpleados) {
+        this.idDepartamento = idDepartamento;
+        this.Nombre = nombre;
+        this.ListaEmpleados = listaEmpleados;
+
+    }
+// constructor vacio
+    public Departamento() {
+    }
+    
+ // getters  and setters
+    public int getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(int idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getListaEmpleados() {
+        return ListaEmpleados;
+    }
+
+    public void setListaEmpleados(String listaEmpleados) {
+        ListaEmpleados = listaEmpleados;
+    }
 }
